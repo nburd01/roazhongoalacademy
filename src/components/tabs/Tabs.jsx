31a2439ed19f751e225abe16732227cdeaae50a4
Niall Burdon <n.burdon@protonmail.com>
2022-08-20 17:51:53 +0200

    added tabs to components with atomic design

---
+++ src/components/tabs/Tabs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './tabs.scss';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Tabs = () => {
+  
+  return (
+    <button
+      
+    >test
+    </button>
+  );
+};
---
+++ src/components/tabs/Tabs.stories.jsx
@@ -0,0 +1,12 @@
+import {Tabs} from './Tabs'
+
+export default {
+    title: 'UI/Tabs',
+    component: Tabs,
+}
+
+export  const Default = (args) =>{
+return(
+    <div>Hello world</div>
+);
+};
