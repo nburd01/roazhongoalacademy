eac50a5234095dada549d52e30fb7d2dfc171400
Niall Burdon <n.burdon@protonmail.com>
2022-09-06 11:51:57 +0200

    added toggle menu

--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
 import {Link } from 'react-router-dom';
 import './MainNav.scss'
 // import logo from '../../assets/img/logo/logo.jpg'
 
 export const MainNav = () => {
+
+  const [toggleNav, setToggleNav] = useState(false)
+
+
   return (
     <div className="MainNav">
-      <img className="logo" src={require('../../assets/img/logo/logo.jpg')} alt="logo" />
-    <nav className="NavMenu">
-        <Link to="/">Home</Link>
-        <Link to="/rga">La RGA</Link>
-        <Link to="/inscriptions">Inscriptions</Link>
-        <Link to="/seances">Seances</Link>
-        <Link to="/multimedia">Multimédia</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login">Login</Link>
-    </nav>
-    <span className="hamburgerMenu">
-      <span></span>
-      <span></span>
-      <span></span>
-    </span>
-  </div>
+      <div className="logoDiv">
+        <img className="logo" src={require('../../assets/img/logo/logo.jpg')} alt="logo" />
+      </div>
+
+<div className={`navbar-links ${toggleNav ? "active" : ""}`}>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/rga">La RGA</a></li>
+            <li><a href="/inscriptions">Inscriptions</a></li>
+            <li><a href="/seances">Seances</a></li>
+            <li><a href="/multimedia">Multimédia</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </div>
+
+      <span className="toggleBtn"  onClick={() => setToggleNav(!toggleNav)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
   )
 }
+
