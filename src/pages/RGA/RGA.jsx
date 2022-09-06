0832b2206c99f145c6946dde429a3e11a9dfe645
Niall Burdon <n.burdon@protonmail.com>
2022-09-06 15:01:22 +0200

    added footer

--- src/pages/RGA/RGA.jsx
+++ src/pages/RGA/RGA.jsx
@@ -6,7 +6,9 @@ export const RGA = () => {
 
       <h1>A propos de la Roazhon Goal Academy</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempore possimus quas. </h3>
-      <img src={require('../../assets/img/galerie/home3.jpg')} className="HeroImage Left" alt="img"/>
+      <div className="HeroImage">
+        <img src={require('../../assets/img/galerie/home3.jpg')} className="_HeroImage Left" alt="img"/>
+      </div>
       <div className="styledUnderlignComp">
         <h6 className="_BgTitle Left">Objectifs</h6>
         <h3 className="_BgSubTitle Left">Objectifs</h3>
--- src/pages/Seances/Seances.jsx
+++ src/pages/Seances/Seances.jsx
@@ -6,7 +6,9 @@ export const Seances = () => {
 
     <h1>Les séances</h1>
     <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempore possimus quas. </h3>
-    <img src={require('../../assets/img/galerie/home4.jpg')} className="HeroImage Left" alt="img"/>
+    <div className="HeroImage">
+        <img src={require('../../assets/img/galerie/home4.jpg')} className="_HeroImage Left" alt="img"/>
+      </div>
       <div className="styledUnderlignComp">
         <h6 className="_BgTitle Left">Séances</h6>
         <h3 className="_BgSubTitle Left">Séance type</h3>
